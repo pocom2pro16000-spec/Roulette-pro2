ef6d4ea0e39162b37e0063e3bae156bf77166528
pocom2pro16000-spec <pocom2pro16000@gmail.com>
2026-03-30 23:38:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
-const grid = document.getElementById('grid');
-const redNumbers = [1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36];
+const numbersContainer = document.getElementById('numbers');
+const redNums = [1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36];
 
-// 1 se 36 tak numbers grid mein dalna
 for (let i = 1; i <= 36; i++) {
-    let div = document.createElement('div');
-    div.innerText = i;
-    div.className = 'num-btn';
-    
-    if (redNumbers.includes(i)) {
-        div.classList.add('red');
-    } else {
-        div.classList.add('black');
-    }
-    
-    div.onclick = () => placeBet(i);
-    grid.appendChild(div);
+    let n = document.createElement('div');
+    n.innerText = i;
+    n.className = 'num ' + (redNums.includes(i) ? 'red' : 'black');
+    n.onclick = () => alert("Bet on " + i);
+    numbersContainer.appendChild(n);
 }
 
-function selectChip(val) {
-    console.log("Selected Chip: " + val);
-    // Yahan chip selection ka logic aayega
-}
-
-function placeBet(num) {
-    alert("Bet placed on: " + num);
-}
+function selectChip(v) { console.log("Chip:", v); }
